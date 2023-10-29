f908410cd498ecf7015f3425af1caea836c6ad07
NatureSL <varistrue@gmail.com>
2023-10-29 22:07:38 +0600

    feat: settings project

---
+++ src/shared/api/api-instance.ts
@@ -0,0 +1,38 @@
+const baseURL = "http://localhost:3000"; // use your own URL here or environment variable
+
+class ApiError extends Error {
+  constructor(public data: unknown) {
+    super("Api Error");
+  }
+}
+
+export const createInstance = async <T>({
+  url,
+  method,
+  params,
+  data,
+}: {
+  url: string;
+  method: "get" | "post" | "put" | "delete" | "patch";
+  params?: Record<string, string>;
+  header?: HeadersInit;
+  data?: BodyType<unknown>;
+  responseType?: string;
+}): Promise<T> => {
+  const response = await fetch(
+    `${baseURL}${url}` + new URLSearchParams(params),
+    {
+      method: method.toUpperCase(),
+      ...(data ? { body: JSON.stringify(data) } : {}),
+    },
+  );
+
+  if (response.status.toString().startsWith("2")) {
+    throw new ApiError(response);
+  }
+  return response.json();
+};
+
+
+export type BodyType<BodyData> = BodyData;
+export type ErrorType<Error> = Error;
